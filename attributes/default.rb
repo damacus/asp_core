8564b494a4ffaf73e29c39b02ea0faba2ea71531
Dan Webb <dan.webb@damacus.io>
2016-12-01 14:07:15 +0000

    Update to 1.0.1 Preview

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,1 @@
-default['asp_net_core']['installer_url'] = 'https://download.microsoft.com/download/5/5/7/557D02A5-C3D0-4EF6-A570-4F75CD0DA5BF/DotNetCore.1.0.1-WindowsHosting.exe'
+default['asp_net_core']['installer_url'] = 'https://download.microsoft.com/download/1/4/1/141760B3-805B-4583-B17C-8C5BC5A876AB/Installers/dotnet-dev-win-x64.1.0.0-preview2-1-003177.exe'
