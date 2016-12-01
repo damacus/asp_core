be02bccd70ff86c8e26a0f72e136500545e1ec6b
Dan Webb <dan.webb@damacus.io>
2016-12-01 11:42:02 +0000

    Change checksum ammount

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 net_core CHANGELOG
 ========================
 0.1.4 (26-09-2016
-Fixes bug with .net core not on the path correlty. 
+Fixes bug with .net core not on the path correctly. 
 
 0.1.3 (14-09-2016)
 Updated DotNetCore package to 1.0.1
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 # rubocop:disable LineLength
 default['asp_net_core']['installer_url'] = 'https://download.microsoft.com/download/5/5/7/557D02A5-C3D0-4EF6-A570-4F75CD0DA5BF/DotNetCore.1.0.1-WindowsHosting.exe'
-default['asp_net_core']['checksum'] = '37f5478bad41f4534830aeceebfc42a1b87ce4e60dccde2552bec91c2371a7fa'
+default['asp_net_core']['checksum'] = 'a6fbdce9bb20'
+a6fbdc
 # rubocop:enable LineLength
